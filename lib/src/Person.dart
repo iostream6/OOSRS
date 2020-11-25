5910bc42d9436710cc1e187b525214e1881cb25f
Ilamah, Osho <oilamah@gmail.com>
2020-11-25 22:04:49 +0300

    Initial commit

---
+++ lib/src/Person.dart
@@ -0,0 +1,16 @@
+/*
+ * 2020.11.25  - Created
+ */
+
+class Person{
+  String name;
+  String ssn;
+
+  Person(this.name, this.ssn);
+
+  Person.createNew(){
+    name ='?';
+    ssn = '???-??-????';
+  }
+
+}
